fc73fd64e37979d0162370e6d3dd6ddf700e467f
SohanShirane <150237835+SohanShirane@users.noreply.github.com>
2023-11-08 15:00:37 +0530

    Health And Fitness Website

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Handle form submission logic here
+    alert("Form submitted! We'll get back to you soon.");
+});
